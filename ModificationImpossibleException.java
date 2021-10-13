61a9f54bf4a74ec1e9a2854934c9580708d14c2b
GentilBastien <bastien.ga@free.fr>
2021-10-14 00:42:35 +0200

    la clique des bg

--- ModificationImpossibleException.java
+++
@@ -1,9 +0,0 @@
-
-public class ModificationImpossibleException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	public ModificationImpossibleException() {
-		super();
-	}
-
-}
---
+++ taquin/Grille.java
@@ -0,0 +1,6 @@
+package taquin;
+
+public class Grille {
+	//I'd like to commit suicide rn
+	//but i need to know how to commit first :D
+}
